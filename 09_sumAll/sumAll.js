061bcd8e1564320c8cd43ab80890f2f1b3356631
Omette <ometteomega@gmail.com>
2025-11-07 06:41:56 -0600

    Solved Exercise 09
    Completed Exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(rangeMin, rangeMax) {
+    let temp;
+    let result = 0;
+    
+    if(rangeMax < 0 || rangeMin < 0 || !(typeof rangeMax === "number") || !(typeof rangeMin === "number")){
+        return "ERROR";
+    }
 
+    if(Math.floor(rangeMax) != rangeMax || Math.floor(rangeMin) != rangeMin){
+        return "ERROR";
+    }
+
+    if(rangeMax < rangeMin){
+        temp = rangeMax;
+        rangeMax = rangeMin;
+        rangeMin = temp;
+    }
+
+
+
+    for(let counter = rangeMin; counter <= rangeMax; counter++){
+        result = result + counter;
+    }
+    return result;
 };
 
 // Do not edit below this line
