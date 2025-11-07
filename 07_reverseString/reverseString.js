7a2770dc32dbd5961be713b1f6656145df93ba74
Omette <ometteomega@gmail.com>
2025-11-07 05:19:46 -0600

    Solved Exercise 07:
    Solved Exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    for (const char of string) {
+        result = char + result;
+    }
+    return result;
 };
 
 // Do not edit below this line
