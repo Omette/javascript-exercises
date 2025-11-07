559f95d8d82388603983b3af5092c1d316b2620b
Omette <ometteomega@gmail.com>
2025-11-07 05:47:50 -0600

    Solved Exercise 08:
    Completed Exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.from(arguments);
+    args.shift();
+
+    let result = [];
+
+    for (const arg of args) {
+        let index = arr.indexOf(arg);
+        while (index != -1) {
+            arr.splice(index, 1);
+            index = arr.indexOf(arg, index);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
